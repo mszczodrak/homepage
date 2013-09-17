ae33017f20045a7396a74ce375a8d491065b4c9a
Marcin <msz@cs.columbia.edu>
2013-09-16 22:29:27 -0400

    banner

--- banner.php
+++ banner.php
@@ -1,36 +1,41 @@
 <table>
-  <tr>
-    <td valign="bottom" width="500">
-      <font size="+2"><b>Marcin K Szczodrak</b></font><br>
-      <br>
-      <font size="-1">
-	467 Computer Science Building<br>
-	1214 Amsterdam Avenue, Mail Code: 0401<br>
-	Columbia University<br>
-	New York, NY 10027-7003<br>
-      </font>
-      <br>
-      <img src="pics/email.jpg" width=240> <br>
-    <td width="100" align="right" valign="bottom">
-      <a href="//plus.google.com/103712325633775186011?prsrc=3"
-        rel="publisher" target="_top" style="text-decoration:none;">
-        <img src="pics/google_plus.png" width="25" alt="Google+"/>
-      </a>
-
-      <a href="http://www.linkedin.com/pub/marcin-szczodrak/31/493/591" 
-         style="text-decoration:none;">
-         <img src="pics/linked_in.png" width="25" alt="LinkedIn">
-      </a>
-      <a href="https://github.com/mszczodrak" 
-         style="text-decoration:none;">
-	 <img src="pics/github_circle_color.png" width="25" alt="GitHub">
-    </a>
-    </td>
-    </td>
-    <td width="200" align="right">
-      <img src="pics/marcin_09_2012.jpg" width=130>
-    </td>
-  </tr>
+	<tr>
+	<td valign="bottom" width="500">
+		<font size="+2"><b>Marcin K Szczodrak</b></font><br>
+		<br>
+		<font size="-1">
+		467 Computer Science Building<br>
+		1214 Amsterdam Avenue, Mail Code: 0401<br>
+		Columbia University<br>
+		New York, NY 10027-7003<br>
+		</font>
+		<br>
+		<img src="pics/email.jpg" width=240> <br>
+	</td>
+	<td width="100" align="right" valign="bottom">
+	<a href="//plus.google.com/103712325633775186011?prsrc=3"
+		rel="publisher" target="_top" style="text-decoration:none;">
+		<img src="pics/google_plus.png" width="25" alt="Google+"/>
+	</a>
+	<a href="http://www.linkedin.com/pub/marcin-szczodrak/31/493/591"
+		style="text-decoration:none;">
+		<img src="pics/linked_in.png" width="25" alt="LinkedIn">
+	</a>
+	<a href="https://github.com/mszczodrak" 
+		 style="text-decoration:none;">
+		<img src="pics/github_circle_color.png" width="25" alt="GitHub">
+	</a>
+	<a>
+	<a href="http://nikeplus.nike.com/plus/profile/mszczodrak/"
+		style="text-decoration:none;">
+		<img src="pics/nike_plus_icon.png" width="25"
+			alt="Nike+ (requires Nike+ account)">
+	</a>
+	</td>
+	<td width="200" align="right">
+		<img src="pics/marcin_09_2012.jpg" width=130>
+	</td>
+	</tr>
 </table>
 
 <p>
