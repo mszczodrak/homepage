caebdfd0f8361a042a1f20a3eb689b41d9f03be5
Marcin Szczodrak <msz@cs.columbia.edu>
2013-10-20 15:36:21 -0400

    update pubs

--- publications.php
+++ publications.php
@@ -6,7 +6,7 @@
 
 <li style="margin-bottom: 10px;">
 <b>Modeling and Implementation of Energy Neutral Sensing Systems</b></br>
-Marcin Szczodrak, Omprakash Gnawali and Luca P Carloni</br>
+Marcin K Szczodrak, Omprakash Gnawali and Luca P Carloni</br>
 Proc. of ENSSys Work., November, 2013.</br>
 <!--<a href="papers/na.html">(PDF)</a>-->
 <a href="papers/szczodrak_enssys13.pdf">(PDF)</a>
@@ -15,7 +15,7 @@ Proc. of ENSSys Work., November, 2013.</br>
 
 <li style="margin-bottom: 10px;">
 <b>An Open Framework to Deploy Heterogeneous Wireless Testbed for Cyber-Physical Systems</b></br>
-Marcin Szczodrak, Yong Yang, Dave Cavalcanti and Luca P Carloni</br>
+Marcin K Szczodrak, Yong Yang, Dave Cavalcanti and Luca P Carloni</br>
 Proc. of IEEE SIES Symp., June, 2013.</br>
 <a href="http://ieeexplore.ieee.org/xpl/articleDetails.jsp?tp=&arnumber=6601494">(IEEE)</a>
 <a href="papers/szczodrak_sies13.pdf">(PDF)</a>
@@ -24,7 +24,7 @@ Proc. of IEEE SIES Symp., June, 2013.</br>
 
 <li style="margin-bottom: 10px;">
 <b>Dynamic Reconfiguration of Wireless Sensor Networks to Support Heterogeneous Applications</b></br>
-Marcin Szczodrak, Omprakash Gnawali and Luca P Carloni</br>
+Marcin K Szczodrak, Omprakash Gnawali and Luca P Carloni</br>
 Proc. IEEE DCOSS Conf., May, 2013.</br>
 <a href="http://ieeexplore.ieee.org/xpl/articleDetails.jsp?tp=&arnumber=6569409">(IEEE)</a>
 <a href="papers/szczodrak_dcoss13.pdf">(PDF)</a>
@@ -34,7 +34,7 @@ Proc. IEEE DCOSS Conf., May, 2013.</br>
 <li style="margin-bottom: 10px;">
 <b>Prototyping Energy Harvesting Active Networked Tags (EnHANTs)</b></br>
 Maria Gorlatova, Robert Margolies, John Sarik, Gerald Stanje, Jianxun Zhu, Baradwaj Vigraham, 
-Marcin Szczodrak, Luca P Carloni, Peter Kinget, Ioannis Kymissis and Gil Zussman.</br>
+Marcin K Szczodrak, Luca P Carloni, Peter Kinget, Ioannis Kymissis and Gil Zussman.</br>
 Proc. IEEE INFOCOM'13 mini-conference, Turin, Italy, April, 2013.</br>
 <a href="http://ieeexplore.ieee.org/xpl/articleDetails.jsp?tp=&arnumber=6566840">(IEEE)</a>
 <a href="papers/gorlatova_infocom13.pdf">(PDF)</a>
@@ -43,7 +43,7 @@ Proc. IEEE INFOCOM'13 mini-conference, Turin, Italy, April, 2013.</br>
 
 <li style="margin-bottom: 10px;">
 <b>Demo: A Complete Framework for Programming Event Driven, Self-Reconfigurable Low Power Wireless Networks</b></br>
-Marcin Szczodrak and Luca P Carloni.</br>
+Marcin K Szczodrak and Luca P Carloni.</br>
 ACM SenSys11, Seattle, WA, November, 2011.</br>
 <a href="http://dl.acm.org/authorize?6503395">(PDF: ACM Author-Izer)</a>
 <a href="http://dl.acm.org/citation.cfm?id=2070942.2071017">(ACM)</a>
@@ -54,7 +54,7 @@ ACM SenSys11, Seattle, WA, November, 2011.</br>
 <li style="margin-bottom: 10px;">
 <b>Demo: Organic Solar Cell-equipped Energy Harvesting Active Networked Tag (EnHANT) Prototypes</b></br>
 Gerald Stanje, Paul Miller, Jianxun Zhu, Alexander Smith, Olivia Winn, Robert Margolies, Maria Gorlatova,
-John Sarik, Marcin Szczodrak, Baradwaj Vigraham, Luca P Carloni, Peter Kinget, Ioannis Kymissis and Gil Zussman.</br>
+John Sarik, Marcin K Szczodrak, Baradwaj Vigraham, Luca P Carloni, Peter Kinget, Ioannis Kymissis and Gil Zussman.</br>
 ACM SenSys11, Seattle, WA, November, 2011.</br>
 <a href="http://dl.acm.org/authorize?6503370">(PDF: ACM Author-Izer)</a>
 <a href="http://dl.acm.org/citation.cfm?id=2070942.2071002">(ACM)</a>
@@ -65,7 +65,7 @@ ACM SenSys11, Seattle, WA, November, 2011.</br>
 <li style="margin-bottom: 10px;">
 <b>Demo: Prototyping UWB-Enabled EnHANTs</b></br>
 Jianxun Zhu, Gerald Stanje, Robert Margolies, Maria Gorlatova, John Sarik, Zainab Noorbhaiwala,
-Paul Miller, Marcin Szczodrak, Baradwaj Vigraham, Luca P Carloni, Peter Kinget, Ioannis Kymissis and Gil Zussman.</br>
+Paul Miller, Marcin K Szczodrak, Baradwaj Vigraham, Luca P Carloni, Peter Kinget, Ioannis Kymissis and Gil Zussman.</br>
 ACM MobiSys11, Washington, DC, June 2011.</br>
 <a href="http://dl.acm.org/authorize?446157">(PDF: ACM Author-Izer)</a>
 <a href="http://dl.acm.org/citation.cfm?id=1999995.2000048">(ACM)</a>
@@ -75,7 +75,7 @@ ACM MobiSys11, Washington, DC, June 2011.</br>
 
 <li style="margin-bottom: 10px;">
 <b>Prototyping Energy Harvesting Active Networked Tags: Phase II MICA Mote-based Devices</b></br>
-Maria Gorlatova, Zainad Noorbhaiwala, Abraham Skolnik, John Sarik, Marcin Szczodrak, Jiasi Chen,
+Maria Gorlatova, Zainad Noorbhaiwala, Abraham Skolnik, John Sarik, Marcin K Szczodrak, Jiasi Chen,
 Michael Zapas, Luca P Carloni, Peter Kinget, Ioannis Kymissis, Dan Rubenstein and Gil Zussman.</br>
 ACM MobiCom'10, Chicago, IL, September 2010</br>
 <a href="papers/gorlatova_mobicom10.pdf">(PDF)</a></br>
@@ -83,7 +83,7 @@ ACM MobiCom'10, Chicago, IL, September 2010</br>
 
 <li style="margin-bottom: 10px;">
 <b>Tiered Architecture for On-line Detection, Isolation, and Repair of Faults in Wireless Sensor Networks</b></br>
-Sadaf Zahedi, Marcin Szczodrak, Ping Ji, Dinkar Mylaraswamy, Mani B Srivastava and Robert Young.</br>
+Sadaf Zahedi, Marcin K Szczodrak, Ping Ji, Dinkar Mylaraswamy, Mani B Srivastava and Robert Young.</br>
 MILCOM, San Diego, CA, November 17-19, 2008</br>
 <a href="http://ieeexplore.ieee.org/xpl/articleDetails.jsp?tp=&arnumber=4753634">(IEEE)</a>
 <a href="papers/zahedi_milcom08.pdf">(PDF)</a></br>
@@ -91,49 +91,49 @@ MILCOM, San Diego, CA, November 17-19, 2008</br>
 
 <li style="margin-bottom: 10px;">
 <b>Simulation Framework for QoI Characterization of Sensor Networks in the Presence of Faults</b></br>
-Marcin Szczodrak, Sadaf Zahedi, Ping Ji, Dinkar Mylaraswamy, Mani B Srivastava and Robert Young.</br>
+Marcin K Szczodrak, Sadaf Zahedi, Ping Ji, Dinkar Mylaraswamy, Mani B Srivastava and Robert Young.</br>
 The Second Annual Conference of the International Technology Alliance, Imperial College London, UK, September 16-18, 2008</br>
 <a href="papers/szczodrak_ita08.pdf">(PDF)</a></br>
 </li>
 
 <li style="margin-bottom: 10px;">
 <b>Multivariate Model for Data Cleansing in Sensor Networks</b></br>
-Ping Ji and Marcin Szczodrak.</br>
+Ping Ji and Marcin K Szczodrak.</br>
 The Second Annual Conference of the International Technology Alliance, Imperial College London, UK, September 16-18, 2008</br>
 <a href="papers/ji_ita08.pdf">(PDF)</a></br>
 </li>
 
 <li style="margin-bottom: 10px;">
 <b>Two-Tier Framework for Sensor Fault Characterization in Sensor Networks</b></br>
-Sadaf Zahedi, Marcin Szczodrak, Ping Ji, Dinkar Mylaraswamy, Mani B Srivastava and Robert Young.</br>
+Sadaf Zahedi, Marcin K Szczodrak, Ping Ji, Dinkar Mylaraswamy, Mani B Srivastava and Robert Young.</br>
 The Second Annual Conference of the International Technology Alliance, Imperial College London, UK, September 16-18, 2008</br>
 <a href="papers/zahedi_ita08.pdf">(PDF)</a></br>
 </li>
 
 <li style="margin-bottom: 10px;">
 <b>Two-Level ZigBee-4G Design for Secure and Efficient Communications in the Resources Constrained Military Environment</b></br>
-Marcin Szczodrak, Jinwoo Kim and Yuncheol Baek.</br>
+Marcin K Szczodrak, Jinwoo Kim and Yuncheol Baek.</br>
 International Journal of Computer Science and Network Security, vol. 7 no.10, October 2007</br>
 <a href="papers/na.html">(PDF)</a></br>
 </li>
 
 <!--
 <li style="margin-bottom: 10px;">
-Marcin Szczodrak. "Review of: "Silence on the Wire. A Field Guide to Passive
+Marcin K Szczodrak. "Review of: "Silence on the Wire. A Field Guide to Passive
 Reconnaissance and Indirect Attack" hackin9, Issue 6/2007 (13), vol.2 no.6
 </li>
 -->
 
 <li style="margin-bottom: 10px;">
 <b>4G and MANET, Wireless Network of Future Battlefield </b></br>
-Marcin Szczodrak and Jinwoo Kim.</br>
+Marcin K Szczodrak and Jinwoo Kim.</br>
 Proceedings of the 2007 International Conference on Security & Management, Las Vegas, Nevada, USA, June 25-28, 2007.</br>
 <a href="papers/na.html">(PDF)</a></br>
 </li>
 
 <li style="margin-bottom: 10px;">
 <b>4GM@4GW: Implementing 4G in the Military Mobile Ad-Hoc Network Environment</b></br>
-Marcin Szczodrak, Jinwoo Kim and Yuncheol Baek.</br>
+Marcin K Szczodrak, Jinwoo Kim and Yuncheol Baek.</br>
 International Journal of Computer Science and Network Security, vol.7 no.4, April 2007</br>
 <a href="papers/na.html">(PDF)</a></br>
 </li>
