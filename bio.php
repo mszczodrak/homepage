05e49cda5092087cee932626cd13a46c6382f793
Marcin Szczodrak <msz@cs.columbia.edu>
2013-10-31 17:32:52 -0400

    update bio

--- bio.php
+++ bio.php
@@ -4,9 +4,11 @@
 Marcin is a Ph.D. Candidate in the <a href="http://www.cs.columbia.edu">
 Computer Science</a> department of the <a href="http://www.columbia.edu">
 Columbia University</a>, working with <a href="http://www.cs.columbia.edu/~luca/">
-Professor Luca Carloni</a>. Marcin's main research interests are low-power 
-and energy-aware embedded systems, wireless sensor and actuator networks, 
-and cyber-physical systems. He has received Bachelor of Science 
+Professor Luca Carloni</a>. Marcin research interests are in the areas of systems, networking,
+and big data. His recent publications are on low-power and energy-aware wireless 
+embedded systems, wireless sensor and actuator networks, and cyber-physical systems,
+with a focus on enabling industrial-level system modeling, development, and installation. 
+He has received Bachelor of Science 
 from <a href="http://www.jjay.cuny.edu">John Jay College of Criminal Justice</a> 
 and Master of Science from the <a href="http://www.ccny.cuny.edu">City College</a>,
 both of the <a href="http://www.cuny.edu">City University of New York</a>.
