17469a147b98a86e73186f27fe8483afa57c3ac5
Marcin Szczodrak <msz@cs.columbia.edu>
2013-11-06 14:35:51 -0500

    up script

--- wsn/scripts/install_tinyos.sh
+++ wsn/scripts/install_tinyos.sh
@@ -5,6 +5,8 @@
 
 INSTALL_DIR=/home/$USER
 PROFILE_FILE=/home/$USER/.profile
+# Version can be either tinyos-main or tinyos-release
+TINYOS_VERSION=tinyos-main
 
 
 # Install Pre-requisites
@@ -57,24 +59,15 @@ else
 fi
 
 cd $INSTALL_DIR/github
-if [ -d tinyos-main ]; then
-	cd tinyos-main
+if [ -d $TINYOS_VERSION ]; then
+	cd $TINYOS_VERSION
 	git pull
 else
-	git clone git@github.com:tinyos/tinyos-main.git
+	git clone git@github.com:tinyos/$TINYOS_VERSION.git
 fi
 
 cd $INSTALL_DIR/github
-if [ -d tinyos-release ]; then
-	cd tinyos-release
-	git pull
-	cd ..
-else
-	git clone git@github.com:tinyos/tinyos-release.git
-fi
-
-echo 'Setting the TinyOS development branch as the default version'
-ln -s tinyos-main tinyos
+ln -s $TINYOS_VERSION tinyos
 
 # Installing nesc from source
 
@@ -85,6 +78,8 @@ cd nesc
 ./configure --prefix=$INSTALL_DIR/tools/nesc
 make
 make install
+
+export PATH=$PATH:$INSTALL_DIR/tools/nesc/bin
 
 # Installing tinyos tools
 echo 'Installing tinyos tools from source code'
@@ -102,13 +97,14 @@ echo 'Installing compilers provided by TinyProd'
 gpg --keyserver keyserver.ubuntu.com --recv-keys 34EC655A
 gpg -a --export 34EC655A | sudo apt-key add -
 
-export TOSPROD="/etc/apt/sources.list.d/tinyprod-debian.list" 
+export TOSPROD="/etc/apt/sources.list.d/tinyprod-debian.list"
 echo "deb http://tinyprod.net/repos/debian squeeze main" | sudo tee $TOSPROD
 echo "deb http://tinyprod.net/repos/debian msp430-46 main" | sudo tee -a $TOSPROD
 
 sudo apt-get update
 sudo apt-get install -y --assume-yes msp430-46
 sudo apt-get install -y --assume-yes avr-tinyos
+sudo apt-get remove -y --assume-yes tinyos-tools
 
 # Set environmental variables
 echo 'Setting environmental variables'
@@ -118,7 +114,7 @@ echo export TOSROOT=$INSTALL_DIR/github/tinyos | sudo tee -a $PROFILE_FILE
 echo export TOSDIR=$INSTALL_DIR/github/tinyos/tos | sudo tee -a $PROFILE_FILE
 echo export CLASSPATH=$INSTALL_DIR/github/tinyos/support/sdk/java/tinyos.jar:. | sudo tee -a $PROFILE_FILE
 echo export MAKERULES=$INSTALL_DIR/github/tinyos/support/make/Makerules | sudo tee -a $PROFILE_FILE
-echo export PATH=$PATH:$INSTALL_DIR/tools/nesc/bin:$INSTALL_DIR/tools/tinyos-tools/bin:$INSTALL_DIR/tools/usr/bin | sudo tee -a $PROFILE_FILE
+echo export PATH=$PATH:$INSTALL_DIR/tools/tinyos-tools/bin:$INSTALL_DIR/tools/usr/bin | sudo tee -a $PROFILE_FILE
 echo export PYTHONPATH=.:$INSTALL_DIR/github/tinyos/support/sdk/python:$PYTHONPATH | sudo tee -a $PROFILE_FILE
 echo export MOTECOM=serial@/dev/ttyUSB0:115200 >> $PROFILE_FILE
 #echo export INTELMOTE2_CONTRIB_DIR=/opt/tinyos-2.x-contrib/intelmote
