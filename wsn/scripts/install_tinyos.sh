d99760efb8b7256a194b8d4078b354e20fb3da2d
Marcin K Szczodrak <marcin@ieee.org>
2014-09-21 20:43:53 -0400

    clean in install_tinyos

--- wsn/scripts/install_tinyos.sh
+++ wsn/scripts/install_tinyos.sh
@@ -93,11 +93,6 @@ cd tinyos/tools
 make
 make install
 
-# Installing compilers
-#echo 'Installing compilers provided by TinyProd'
-#gpg --keyserver keyserver.ubuntu.com --recv-keys 34EC655A
-#gpg -a --export 34EC655A | sudo apt-key add -
-
 export TOSSTAN="/etc/apt/sources.list.d/tinyon-stanford-debian.list"
 echo "deb http://tinyos.stanford.edu/tinyos/dists/ubuntu natty main" | sudo tee $TOSSTAN
 
@@ -110,14 +105,10 @@ sudo apt-get remove -y --assume-yes tinyos-tools
 echo 'Setting environmental variables'
 echo " " | sudo tee -a $PROFILE_FILE
 echo "# Set the envoronment variables for TinyOS" | sudo tee -a $PROFILE_FILE
-#echo export TOSROOT=$INSTALL_DIR/github/tinyos | sudo tee -a $PROFILE_FILE
-#echo export TOSDIR=$INSTALL_DIR/github/tinyos/tos | sudo tee -a $PROFILE_FILE
 echo export CLASSPATH=$INSTALL_DIR/github/tinyos/support/sdk/java/tinyos.jar:. | sudo tee -a $PROFILE_FILE
-#echo export MAKERULES=$INSTALL_DIR/github/tinyos/support/make/Makerules | sudo tee -a $PROFILE_FILE
 echo export PATH=$PATH:$INSTALL_DIR/tools/tinyos-tools/bin:$INSTALL_DIR/tools/usr/bin | sudo tee -a $PROFILE_FILE
 echo export PYTHONPATH=.:$INSTALL_DIR/github/tinyos/support/sdk/python:$PYTHONPATH | sudo tee -a $PROFILE_FILE
 echo export MOTECOM=serial@/dev/ttyUSB0:115200 >> $PROFILE_FILE
-#echo export INTELMOTE2_CONTRIB_DIR=/opt/tinyos-2.x-contrib/intelmote
 echo " " | sudo tee -a $PROFILE_FILE
 
 echo "Run "
